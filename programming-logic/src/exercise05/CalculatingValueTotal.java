e4e0d89215be3a8424361f6171f5932637bf5083
hywenklis <hywenklis@hotmail.com>
2022-08-04 22:15:33 -0300

    Exercicio - 05: Encontre o valor total baseado na quantidade e no desconto

---
+++ programming-logic/src/exercise05/CalculatingValueTotal.java
@@ -0,0 +1,30 @@
+package exercise05;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Scanner;
+
+public class CalculatingValueTotal {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the product value: ");
+        BigDecimal productValue = scanner.nextBigDecimal();
+
+        System.out.println("Enter the quantity of the product you want: ");
+        int quantityProduct = scanner.nextInt();
+
+        BigDecimal subTotal = productValue.multiply(BigDecimal.valueOf(quantityProduct));
+
+        System.out.println("The value of your purchase is: $" + subTotal);
+
+        BigDecimal discountSubtotal;
+
+        if (quantityProduct >= 10) {
+            discountSubtotal = subTotal.divide(BigDecimal.valueOf(10), 2, RoundingMode.UP);
+            System.out.println("A 10% discount has been applied to your purchase its total value is: $" + discountSubtotal);
+        }
+    }
+}
